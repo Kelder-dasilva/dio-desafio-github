991d9ad1da953b4852ff95fbdbac1cb2f2d4f5fe
Kelder-dasilva <kelderdasilva10@hotmail.com>
2022-06-28 10:55:30 -0300

    desafios da dio, um é verificação de palindromo e o segundo de substituição de numero par em array

---
+++ dio-javascript-3/atividade1.js
@@ -0,0 +1,22 @@
+// PALINDROMO 
+
+// SOLUÇAO 1
+function verificaPalindromo(string) {
+    if (!string) return "string inexistente";
+
+    return string.split("").reverse().join("") === string;
+}
+console.log(verificaPalindromo("ovo"));
+
+// SOLUÇAO 2
+function verificaPalindromo2(string) {
+    if (!string) return "string inexistente";
+
+    for (let i = 0; i < string.length / 2; i++) {
+        if (string[i] !== string[string.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(verificaPalindromo2("eu"));
